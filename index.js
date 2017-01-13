83400558fcb9e9dbc5eaa02d4ea9c794e3c7335b
dmuktesh <muktesh@idyllic-software.com>
2017-01-13 18:47:07 +0530

    random number module created

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var http = require('http');
 var mustache = require('mustache');
+var randomnum = require('./random_number')
 requestHandler = function(req, res) {
 	console.log('handler');
 
 	if (req.url == '/') {
-result = mustache.render("Hello {{first_name}} {{last_name}}", 
-			  {
-			    "first_name": 'muktesh',
-			    "last_name": 'deshpande'
-			  }
-			)
-			res.end("welcome home"+result);
+			result = mustache.render("Hello {{first_name}} {{last_name}}", 
+						  {
+						    "first_name": 'muktesh',
+						    "last_name": 'deshpande'
+						  }
+						)
+			random1 = randomnum();
+			res.end("welcome home"+result+" "+random1);
 	} else {
 		res.end('hii');
 	}
---
+++ random_number.js~
@@ -0,0 +1,5 @@
+var MAX_NUM = 100
+randomNumber = function(){
+return Math.floor(Math.random()*MAX_NUM)
+}
+module.exports = randomNumber;
